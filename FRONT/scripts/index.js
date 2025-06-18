a7043cf2a00eef1b9c918284b992e4366013d72a
monti-py <bmuzzanti@pioix.edu.ar>
2025-06-18 08:46:39 -0300

    anda registrar

--- FRONT/scripts/index.js
+++ FRONT/scripts/index.js
@@ -5,12 +5,15 @@ const BtnSignUp= document.getElementById("btn-crear-cuenta")
 const modalSignIn= document.getElementById("modal-register")
 const btnCloseSignIn = document.getElementById("btn-dms-register")
 const formLogin = document.getElementById("form-login")
-const formRegister = document.querySelector("form-register")
+const formRegister = document.getElementById("form-register")
 const btnRanking = document.getElementById("btn-ranking")
 
 btnLogin.addEventListener("click",()=>{
     ui.showLoginModal()
+    formLogin.style.display = "flex"
+
 })
+
 
 btnCloseModal.addEventListener("click",()=>{
     ui.closeLoginModal()
@@ -18,7 +21,8 @@ btnCloseModal.addEventListener("click",()=>{
 
 BtnSignUp.addEventListener("click",()=>{
     ui.showSignInModal()
-})
+    formRegister.style.display = "flex"
+}) 
 
 btnCloseSignIn.addEventListener("click",()=>{
     ui.closeSignInModal()
@@ -36,6 +40,20 @@ formLogin.addEventListener("submit",(e)=>{
     formLogin.reset()
 })
 
+formRegister.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    modalSignIn.close()
+    const usuario = {
+        correo_electronico: ui.getMailSignIn(),
+        contraseña: ui.getPasswordSignIn(),
+        nombre: ui.getNameSignIn(),
+        es_admin: 0
+    }
+    console.log(usuario.email)
+    registrarUsuario(usuario)
+    formRegister.reset()
+})
+
 async function verificarUsuario(usuario) {
     const response = await fetch(`http://localhost:4000/verificarUsuario?correo_electronico=${usuario.email}&contraseña=${usuario.password}`,{
         method: "GET",
@@ -48,4 +66,24 @@ async function verificarUsuario(usuario) {
     console.log(result)
     ui.DoLogin(result)
     return result
+}
+
+async function registrarUsuario(usuario) {
+  const response = await fetch("http://localhost:4000/registrarUsuario", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(usuario),
+  });
+
+  console.log(response);
+  let result = await response.json();
+  console.log(result)
+  if (result.mensaje == "ok") {
+      ui.DoLogin(usuario); 
+    
+  } else {
+   alert("Algo salio mal") 
+  }
 }
